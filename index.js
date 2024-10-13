cc4e4edf5018e92520fdab5b549e5df0fbf461c1
sirajulnoman7 <mdsirajulnoman@gmail.com>
2024-10-13 13:51:05 +0600

    host vercel

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 const express = require('express')
 const cors = require('cors')
-const jwt=require('jsonwebtoken')
+const jwt = require('jsonwebtoken')
 
 const cookieParser = require('cookie-parser')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 
-app.options('*', cors({
-  origin: 'http://localhost:5173',
-  credentials: true
-}));
+
 
 
 require('dotenv').config()
@@ -21,8 +18,10 @@ const port = process.env.PORT || 5000
 // middleware
 app.use(cookieParser());
 app.use(cors({
-  origin:['http://localhost:5173'],
-  credentials:true
+  origin: ['https://car-doctors-client-d4f85.web.app',
+      'https://car-doctors-client-d4f85.firebaseapp.com'
+  ],
+  credentials: true
 }));
 app.use(express.json())
 
@@ -46,150 +45,156 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
-     const serviceCollection=client.db("carDoctor").collection("services")
-     const checkoutCollection=client.db("carDoctor").collection("booking")
-
-
-// authentication jwt
+    const serviceCollection = client.db("carDoctor").collection("services")
+    const checkoutCollection = client.db("carDoctor").collection("booking")
 
 
-// Set the JWT as an HTTP-only cookie
+    // authentication jwt
 
 
-// app.post('/jwt',async(req,res)=>{
-//     const user=req.body
-
-//     console.log(user)
-    
-//     const token=  jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,{ expiresIn: '1h' })
-//     res.cookie('token',token,{
-//       secure: false,
-//      httpOnly: true,
-//      sameSite:'lax'
-//     }).send({success:true})
-// })
-
-// ({
-//   origin: 'http://localhost:5173',  // Allow requests from your React app
-//   credentials: true  // Allow cookies to be sent with requests
-// })
+    // Set the JWT as an HTTP-only cookie
 
 
+    // app.post('/jwt',async(req,res)=>{
+    //     const user=req.body
 
-app.post('/jwt',(req,res)=>{
-  const user=req.body
-  console.log(user)
-  const token=jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn: '1h'})
-  res.cookie('token',token,{
-    secure:false,
-    httpOnly:true,
-    sameSite:'lax'
-  }).send({success:true})
-})
+    //     console.log(user)
+
+    //     const token=  jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,{ expiresIn: '1h' })
+    //     res.cookie('token',token,{
+    //       secure: false,
+    //      httpOnly: true,
+    //      sameSite:'lax'
+    //     }).send({success:true})
+    // })
+
+    // ({
+    //   origin: 'http://localhost:5173',  // Allow requests from your React app
+    //   credentials: true  // Allow cookies to be sent with requests
+    // })
 
 
-  // middleware create 
-  
-  const verifyToken= async(req,res,next)=>{
-     const token=req.cookies?.token;
-    //  console.log('verify middleware',token)
-     if(!token){
-      return res.status(401).send({message:'Unauthorized'})
-     }
+    //  auth related api explore jwt
+    app.post('/jwt', (req, res) => {
+      const user = req.body
+      console.log(user)
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+      res.cookie('token', token, {
+        // secure: false,
+        secure: true,
+        httpOnly: true,
+        sameSite: 'lax'
+      }).send({ success: true })
+    })
+    app.post('/logout', async (req, res) => {
+      const user = req.body
+      console.log(user)
+      res.clearCookie('token',{ maxAge: 0},).send({success:true})
+    })
 
-     jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+
+    // middleware create 
+
+    const verifyToken = async (req, res, next) => {
+      const token = req.cookies?.token;
+      //  console.log('verify middleware',token)
+      if (!token) {
+        return res.status(401).send({ message: 'Unauthorized' })
+      }
+
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
         // error 
-        if(err){
+        if (err) {
           console.log(err)
-          return res.status(401).send({message:'Unauthorized you have not any access'})
+          return res.status(401).send({ message: 'Unauthorized you have not any access' })
         }
         // decoded
-        console.log('verify token jwt',decoded)
-        req.user=decoded;
+        console.log('verify token jwt', decoded)
+        req.user = decoded;
         next()
-     })
-    
-    
-  }
-
-  const logger= async(req,res,next)=>{
-        // console.log('my middleware',req.host, req.originalUrl)
-    next()
-
-  }
- 
-     app.get("/service",logger, async(req,res)=>{
-        const cursor = serviceCollection.find();
-        const result=await cursor.toArray()
-        res.send(result)
-
-     })
+      })
 
 
-    
+    }
 
-     app.get("/service/:id",logger, async(req,res)=>{
-        const id=req.params.id
-        const query = { _id: new ObjectId(id)}
-        const options = {
-          
-      
-            
-            // Include only the `title` and `imdb` fields in the returned document
-            projection: { price: 1 ,description:1,service_id:1,img:1,title:1,},
-          };
-        const result=await serviceCollection.findOne(query,options)
-        res.send(result)
-     })
+    const logger = async (req, res, next) => {
+      // console.log('my middleware',req.host, req.originalUrl)
+      next()
+
+    }
+
+    app.get("/service", logger, async (req, res) => {
+      const cursor = serviceCollection.find();
+      const result = await cursor.toArray()
+      res.send(result)
+
+    })
+
+
+
+
+    app.get("/service/:id", logger, async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const options = {
+
+
+
+        // Include only the `title` and `imdb` fields in the returned document
+        projection: { price: 1, description: 1, service_id: 1, img: 1, title: 1, },
+      };
+      const result = await serviceCollection.findOne(query, options)
+      res.send(result)
+    })
 
     //   filter email user
     // http://localhost:5000/booking?email=sirajulll1213@gmail.com&sort=1  (finding formula) 
-     app.get('/booking',logger, verifyToken, async(req,res)=>{
+    app.get('/booking', logger, verifyToken, async (req, res) => {
       // console.log("cookie token ",req.cookies.token)
-      
+
       // jwt verify access token and user 
-      console.log('form valid token user',req.user)
-      if(req.query.email !==req.user.email){
-        return res.status(403).send({message:"permission not alow "})
+      console.log('form valid token user', req.user)
+      if (req.query.email !== req.user.email) {
+        return res.status(403).send({ message: "permission not alow " })
       }
-        let query={}
-        if(  req.query?.email){
-            query={email: req.query?.email}
-        }
-        const result=await checkoutCollection.find(query).toArray()
-        res.send(result)
-     })
-           
-     app.post('/booking',async(req,res)=>{
-           const data=req.body
-           console.log(data)
-           const result = await checkoutCollection.insertOne(data);
-           res.send(result)
-     })
+      let query = {}
+      if (req.query?.email) {
+        query = { email: req.query?.email }
+      }
+      const result = await checkoutCollection.find(query).toArray()
+      res.send(result)
+    })
 
-     app.put('/booking/:id',async(req,res)=>{
-        const updateBooking=req.body
-        const id=req.params.id
-        const filter={ _id: new ObjectId(id)}
-        console.log(updateBooking)
-        const updateDoc = {
-            $set: {
-              status:updateBooking.status
-            },
-          };
-          const result = await checkoutCollection.updateOne(filter, updateDoc);
-          res.send(result)
+    app.post('/booking', async (req, res) => {
+      const data = req.body
+      console.log(data)
+      const result = await checkoutCollection.insertOne(data);
+      res.send(result)
+    })
 
-     })
+    app.put('/booking/:id', async (req, res) => {
+      const updateBooking = req.body
+      const id = req.params.id
+      const filter = { _id: new ObjectId(id) }
+      console.log(updateBooking)
+      const updateDoc = {
+        $set: {
+          status: updateBooking.status
+        },
+      };
+      const result = await checkoutCollection.updateOne(filter, updateDoc);
+      res.send(result)
 
-     app.delete('/booking/:id',async(req,res)=>{
-        const id=req.params.id
-        const query = { _id: new ObjectId(id) };
-         const result = await checkoutCollection.deleteOne(query);
-         res.send(result)
-     })
+    })
+
+    app.delete('/booking/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) };
+      const result = await checkoutCollection.deleteOne(query);
+      res.send(result)
+    })
 
 
     // Send a ping to confirm a successful connection
